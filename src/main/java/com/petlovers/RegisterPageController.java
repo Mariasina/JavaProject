a1909b3bee62d42c9d7ac21c65462504ab5d9442
Mariasina <carolbeveer@gmail.com>
2024-05-28 16:18:39 -0300

    register controller being worked on

--- src/main/java/com/petlovers/RegisterPageController.java
+++ src/main/java/com/petlovers/RegisterPageController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -24,5 +25,15 @@ public class RegisterPageController {
     @FXML
     protected TextField tfFullName;
     @FXML
+    protected TextField tfUserName;
+    @FXML
+    protected DatePicker dpBithDate;
+    @FXML
+    protected TextField tfUserCPF;
+    @FXML
     protected PasswordField psUserPass;
+    @FXML
+    protected TextField tfUserEmail;
+    @FXML
+    protected TextField tfUserPhone;
 }
